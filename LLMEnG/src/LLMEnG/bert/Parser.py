5c9c2deb878dac8c1b4473a2bc103d4dd1a10c1a
PoPse <1967439427@qq.com>
2024-09-08 19:23:08 +0800

    tt

--- LLMEnG/src/LLMEnG/bert/Parser.py
+++ LLMEnG/src/LLMEnG/bert/Parser.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description="A simple LLM.")
 '''
 parser.add_argument("--device", type=str, default="cuda:1", choices=['cpu', 'cuda:0', 'cuda:1'], help="GPU设备")
 # 数据
-parser.add_argument("--datasets_json", type=str, default="/home/btr/bpmn/LLMEnG/my-data/datasets8-llama3.1-70B.json", help="JSON格式的数据集路径")
+parser.add_argument("--datasets_json", type=str, default="/home/user/baitianrui/bpmn/LLMEnG/my-data/datasets8-llama3.1-70B.json", help="JSON格式的数据集路径")
 parser.add_argument("--batch_size", type=int, default="1", help="批量大小")
 parser.add_argument("--shuffle", type=bool, default="False", help="是否打乱数据")
 # 模型
@@ -17,10 +17,10 @@ parser.add_argument("--fusion_method", type=str, default="concat", choices=['con
 parser.add_argument("--hidden_size", type=int, default="128", help="隐藏层大小")
 parser.add_argument("--aggr", type=str, default="mean", choices=['mean', 'lstm'], help="GraphSAGE聚合函数")
 parser.add_argument("--lr", type=float, default="0.01", help="学习率")
-parser.add_argument("--epochs", type=int, default="150", help="训练轮数")
+parser.add_argument("--epochs", type=int, default="2", help="训练轮数")
 
 # token embedding
-parser.add_argument("--llm_model", type=str, default="/home/btr/bpmn/model/safetensors/bert-base-uncased", help="LLM模型路径")
+parser.add_argument("--llm_model", type=str, default="/home/user/baitianrui/bpmn/model/safetensors/bert-base-uncased", help="LLM模型路径")
 parser.add_argument("--the_way_of_emb_new_token", type=str, default="mean", choices=['random', 'mean', 'zero'], help="新token的嵌入方式: random | mean | zero | copy")
 parser.add_argument("--the_way_of_token_emb", type=str, default="sum", choices=['cls'], help="token嵌入的获取方式: cls(使用CLS作为token嵌入) | mean(使用最后一层模型输出，求平均) | pooler_output(bert模型的输出)")
 
